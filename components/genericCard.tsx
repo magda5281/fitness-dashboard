4c9158445f291a01daef2441182b62da1f7ac464
Magdalena Gozdalik <magdagozdalik28@gmail.com>
2025-04-07 14:08:59 +0100

    Add initial layout for dashboard page

---
+++ components/genericCard.tsx
@@ -0,0 +1,33 @@
+import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card';
+import { cn } from '@/lib/utils';
+
+interface DashboardCardProps extends React.HTMLAttributes<HTMLDivElement> {
+  title: string;
+  description?: string;
+  className?: string;
+  children: React.ReactNode;
+}
+
+export function GenericCard({
+  title,
+  description,
+  className,
+  children,
+  ...props
+}: DashboardCardProps) {
+  return (
+    <Card className={cn('p-4 gap-4 md:p-6 md:gap-6', className)} {...props}>
+      <CardHeader className='p-0'>
+        <CardTitle className='text-sm sm:text-base md:text-lg font-medium'>
+          {title}
+        </CardTitle>
+        {description && (
+          <p className='text-xs sm:text-sm md:text-base text-muted-foreground'>
+            {description}
+          </p>
+        )}
+      </CardHeader>
+      <CardContent className='flex-1 p-0'>{children}</CardContent>
+    </Card>
+  );
+}
