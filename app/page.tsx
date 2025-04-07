6d4c36594ba36aa71c3cff01e61db4f64661cab9
Magdalena Gozdalik <magdagozdalik28@gmail.com>
2025-04-07 18:47:20 +0100

    Add KPI Cards Row

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
+import { StepsChart } from '@/components/charts/stepsChart';
 import { GenericCard } from '@/components/genericCard';
+import { getFitnessData } from '@/lib/data';
+import { findByDate } from '@/lib/utils';
 import { AvatarIcon } from '@radix-ui/react-icons';
+import { Droplet, Flame, Footprints, Heart } from 'lucide-react';
 
-export default function DashboardPage() {
+export default async function DashboardPage() {
+  const fitnessData = await getFitnessData();
+  const today = new Date().toISOString().split('T')[0];
+  const currentSteps = findByDate(fitnessData.steps, today);
+  const heartHealth = findByDate(fitnessData.heartRateData, today);
+  const hydration = findByDate(fitnessData.hydration, today);
+  const calories = findByDate(fitnessData.calories, today);
+
   return (
     <main className='flex min-h-screen flex-col  p-4 md:p-6 lg:p-8'>
       <div className='flex items-center justify-between'>
@@ -11,31 +22,50 @@ export default function DashboardPage() {
         {/* KPI Cards Row */}
         <div className='grid grid-cols-2  lg:grid-cols-4 gap-4 md:gap-6'>
           <GenericCard title="Today's Steps" description='+12% from yesterday'>
-            <div className='md:text-2xl font-bold '>8,432</div>
+            <div className='md:text-2xl font-bold flex items-center justify-center gap-2 md:gap-4'>
+              <Footprints /> {currentSteps?.steps.toLocaleString()}
+            </div>
           </GenericCard>
-          <GenericCard title="Today's Steps" description='+12% from yesterday'>
-            <div className='md:text-2xl font-bold '>8,432</div>
+          <GenericCard title='Heart health' description='+10% from yesterday'>
+            <div className='md:text-2xl font-bold flex items-center justify-center gap-2 md:gap-4'>
+              <Heart color='red' />
+              {heartHealth?.avgRestingHR} Bpm
+            </div>
           </GenericCard>
-          <GenericCard title="Today's Steps" description='+12% from yesterday'>
-            <div className='md:text-2xl font-bold '>8,432</div>
+          <GenericCard
+            title='Hydration'
+            description={` ${(
+              (hydration?.goal ?? 3) - (hydration?.current ?? 0)
+            ).toLocaleString()} ${hydration?.unit ?? ''} to go`}
+          >
+            <div className='md:text-2xl font-bold flex items-center justify-center gap-2 md:gap-4'>
+              <Droplet color='blue' />
+              {`${hydration?.current} ${hydration?.unit}`}
+            </div>
           </GenericCard>
-          <GenericCard title="Today's Steps" description='+12% from yesterday'>
-            <div className='md:text-2xl font-bold '>8,432</div>
+          <GenericCard title='Calories' description='Consumed vs Burned'>
+            <div className='md:text-2xl font-bold flex items-center justify-center gap-2 md:gap-4'>
+              <Flame color='orange' />
+              {`${(calories?.consumed ?? 0) - (calories?.burned ?? 0)} ${
+                calories?.unit ?? ''
+              }`}
+            </div>
           </GenericCard>
         </div>
 
         {/* Charts Grid */}
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
           {/* Example Chart 1 */}
+
           <GenericCard title='Step Activity' className='col-span-1'>
-            <div className=''>Chart</div>
+            <StepsChart data={fitnessData.steps} />
           </GenericCard>
 
           {/* Example Chart 2 - With description */}
           <GenericCard
             title='Heart Rate'
             description='Last 7 days average'
-            className='col-span-1 md:col-span-2 lg:col-span-1'
+            className='col-span-1 '
           >
             <div className=''>Chart</div>
           </GenericCard>
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,14 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+// A generic helper to find today's entry in any array of objects that have a `date` string
+export function findByDate<T extends { date: string }>(
+  arr: T[],
+  date: string
+): T | undefined {
+  return arr.find((item) => item.date === date);
 }
