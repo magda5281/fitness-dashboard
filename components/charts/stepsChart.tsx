ad22e98ed733dac332e529c85f5ac78e31959e94
Magdalena Gozdalik <magdagozdalik28@gmail.com>
2025-04-08 18:15:48 +0100

    Fixed window error in stepsChart

--- components/charts/stepsChart.tsx
+++ components/charts/stepsChart.tsx
@@ -1,22 +1,27 @@
 'use client';
 import { formatDate } from '@/lib/utils';
+import type { StepsData } from '@/types';
+import { useEffect, useState } from 'react';
 import {
   BarChart,
   Bar,
   XAxis,
   ResponsiveContainer,
   YAxis,
-  Legend,
   Tooltip,
   CartesianGrid,
   LabelList,
 } from 'recharts';
 
-interface StepsChartProps {
-  data: { date: string; steps: number }[];
-}
+export function StepsChart({ data }: { data: StepsData }) {
+  const [isMobile, setIsMobile] = useState(false);
 
-export function StepsChart({ data }: StepsChartProps) {
+  // Safely access `window` in useEffect
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      setIsMobile(window.innerWidth < 768);
+    }
+  }, []);
   const formattedData = data.map((item) => ({
     ...item,
     date: formatDate(item.date),
@@ -26,22 +31,35 @@ export function StepsChart({ data }: StepsChartProps) {
     <ResponsiveContainer width='100%' height='100%'>
       <BarChart
         data={formattedData}
-        margin={{ top: 10, right: 0, left: 0, bottom: 10 }}
+        margin={{ top: 10, right: 0, left: 0, bottom: isMobile ? 10 : 20 }}
         layout='horizontal'
       >
         <CartesianGrid strokeDasharray='3 3' />
         <XAxis
           dataKey='date'
-          tick={{ fontSize: 8 }}
+          tick={{ fontSize: 'clamp(0.5rem, 2vw, 0.75rem)' }}
           interval={0}
           minTickGap={0}
-          angle={-45} // Rotate labels
+          angle={-45}
           textAnchor='end'
         />
         <YAxis tick={{ fontSize: 8 }} interval={0} width={40} />
-        <Tooltip />
+        <Tooltip
+          contentStyle={{
+            fontSize: 'clamp(0.75rem, 1vw, 1rem)',
+            padding: 'clamp(4px, 1vw, 8px)',
+            color: '#333',
+            backgroundColor: '#fff',
+            border: '1px solid #ddd',
+            borderRadius: '4px',
+          }}
+        />
         <Bar dataKey='steps' fill='#8884d8' radius={[4, 4, 0, 0]}>
-          <LabelList dataKey='steps' position='top' fontSize={8} />
+          <LabelList
+            dataKey='steps'
+            position='top'
+            fontSize='clamp(0.5rem, 2vw, 0.75rem)'
+          />
         </Bar>
       </BarChart>
     </ResponsiveContainer>
