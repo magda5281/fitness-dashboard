e3792d0b4d832648f78083925465308073e99c9a
Magdalena Gozdalik <magdagozdalik28@gmail.com>
2025-04-09 20:00:19 +0100

    Add header bg

--- app/page.tsx
+++ app/page.tsx
@@ -72,7 +72,7 @@ export default async function DashboardPage() {
 
   return (
     <main className='flex gap-4 min-h-screen flex-col'>
-      <div className='sticky top-0 z-50 bg-background p-4 sm:p-5 md:p-6 lg:p-8 xl:p-10 border-b'>
+      <div className='sticky top-0 z-50 bg-[var(--data-green)] text-white p-4 sm:p-5 md:p-6 lg:p-8 xl:p-10 border-b'>
         <div className='flex items-center justify-between max-w-7xl mx-auto'>
           <h1 className='font-bold text-lg sm:text-xl md:text-2xl lg:text-3xl'>
             Fitness & Health Dashboard
