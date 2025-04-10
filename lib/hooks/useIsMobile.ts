5082a702ea94505b8f086db27eaae4e32da42ccb
Magdalena Gozdalik <magdagozdalik28@gmail.com>
2025-04-10 10:36:35 +0100

    Add documentation

--- lib/hooks/useIsMobile.ts
+++ lib/hooks/useIsMobile.ts
@@ -1,5 +1,11 @@
 import { useState, useEffect } from 'react';
 
+/**
+ *
+ * @param breakpoint - The width in pixels at which the layout should switch from mobile to desktop.
+ * Default is 768 pixels.
+ * @returns
+ */
 export function useIsMobile(breakpoint: number = 768): boolean {
   const [isMobile, setIsMobile] = useState(false);
 
